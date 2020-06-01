2d5881a4a1a2f3028f2abda063ac63176a76ab43
djtinkers <djtinkers365@gmail.com>
2020-05-31 22:42:05 -0700

    version 3 complete

--- clock.0.3.py
+++ clock.0.3.py
@@ -5,48 +5,108 @@ class Clock:
         # start clock
         # print time every second
         # check for alarm every interval
-    def newAlarm():
+    def newAlarm(User,Alarm):
         pass;
-        # use Alarm object
-    def deleteAlarm():pass;
-    def getAlarm():pass;
+        # use User Class
+        # no duplicate users
+        # use Alarm Class
+        # merge-store/zipOBJ User & Alarm
+    def deleteAlarm(User,Alarm):
+        pass;
+        # find alarm
+        # exist-alarm   delete alarm
+        # !exist:       report error
 
-    def newUser():pass;
-    def deleteUser():pass;
-    def getUser():pass;
+    def getAlarms(User):
+        # find alarms matching user
+        # exist-alarmuser:  print found alarms
+        # !exist-alarmuser: report error
+
+    def newUser(User):
+        pass;
+        # use User class
+        # exist-user:   create
+        # user exist:   report error
+    def deleteUser(User):
+        pass;
+        # find user
+        # exist-user    : delete user
+        # !exist-user   : report error
+    def getUser(User):
+        pass;
+        # find user
+        # exist-user    : print user
+        # !exist-user   : report error
 
 class Date:
     """ A simple date obj interface {hour,minute,second} """
-    def __init__(self,month,day,year):pass;
+    def __init__(self,month,day,year):
+        pass;
+        # mirror params to self
+
 
     @staticmethod
-	def checkMonth(month):pass;
+	def checkMonth(month):
+        pass;
+        # 12 >= month >= 1
+        # true: return month;
+        # false: raise error
 
 	@staticmethod
-	def checkDay(day):pass;
+	def checkDay(day):
+        pass;
+        # 31 >= day >= 1   # pretend all months have 31 days
+        # true: return month;
+        # false: raise error
 
 	@staticmethod
-	def checkYear(year):pass;
+	def checkYear(year):
+        pass;
+        # year >= actual year
+        # true:     return year
+        # false:    raise error
 
 class Time:
     """ A simple time obj interface {hour,minute,second}"""
-    def __init__(self,hour,minute,second):pass;
+    def __init__(self,hour,minute,second):
+        pass;
+        # mirror params to self
+        # check args
 
     @staticmethod
-	def checkHour(hour):pass;
+	def checkHour(hour):
+        pass;
+        # 23 >= hour >= 0
+        # true:     return hour
+        # false:    raise error
 
 	@staticmethod
-	def checkMinute(minute):pass;
+	def checkMinute(minute):
+        pass;
+        # 23 >= minute >= 0
+        # true:     return minute
+        # false:    raise error
 
 	@staticmethod
-	def checkSecond(second):pass;
+	def checkSecond(second):
+        pass;
+        # 59 >= second >= 0
+        # true:     return second
+        # false:    raise error
 
 class Alarm:
     """ A simple alarm object interface {dateOBJ,timeOBJ} """
-    def __init__(self,dateOBJ,timeOBJ):pass;
+    def __init__(self,dateOBJ,timeOBJ):
+        pass;
+        # mirror params to self
+        # check args
 
     @staticmethod
-    def checkDateOBJ(dataOBJ):pass;
+    def checkDateOBJ(dataOBJ):
+        pass;
+        # 23 >= hour >= 0
+        # true:     return hour
+        # false:    raise error
 
     @staticmethod
     def checkTimeOBJ(timeOBJ):pass;
@@ -54,10 +114,21 @@ class Alarm:
 
 class User:
     """ A simple user object interface {username,password} """
-    def __init__(self,username,password):pass;
+    def __init__(self,username,password):
+        pass;
+        # mirror params to self
+        # check args
 
     @staticmethod
-    def checkUsername(username):pass;
+    def checkUsername(username):
+        pass;
+        # notEmpty-username:
+        # true:    return username
+        # false:   raise error
 
     @staticmethod
-    def checkPassword(password):pass;
+    def checkPassword(password):
+        pass;
+        # notEmpty-username:
+        # true:    return username
+        # false:   raise error
