0c63672140b46468b8802ec344c6e8631467b945
djtinkers <djtinkers365@gmail.com>
2020-05-31 21:41:56 -0700

    version 2 complete

---
+++ clock.0.2.py
@@ -0,0 +1,58 @@
+class Clock:
+    """A simple clock written in python"""
+    def __init__(self):pass;
+
+    def newAlarm():pass;
+    def deleteAlarm():pass;
+    def getAlarm():pass;
+
+    def newUser():pass;
+    def deleteUser():pass;
+    def getUser():pass;
+
+class Date:
+    """ A simple date obj interface {hour,minute,second} """
+    def __init__(self,month,day,year):pass;
+
+    @staticmethod
+	def checkMonth(month):pass;
+
+	@staticmethod
+	def checkDay(day):pass;
+
+	@staticmethod
+	def checkYear(year):pass;
+
+class Time:
+    """ A simple time obj interface {hour,minute,second}"""
+    def __init__(self,hour,minute,second):pass;
+
+    @staticmethod
+	def checkHour(hour):pass;
+
+	@staticmethod
+	def checkMinute(minute):pass;
+
+	@staticmethod
+	def checkSecond(second):pass;
+
+class Alarm:
+    """ A simple alarm object interface {dateOBJ,timeOBJ} """
+    def __init__(self,dateOBJ,timeOBJ):pass;
+
+    @staticmethod
+    def checkDateOBJ(dataOBJ):pass;
+
+    @staticmethod
+    def checkTimeOBJ(timeOBJ):pass;
+
+
+class User:
+    """ A simple user object interface {username,password} """
+    def __init__(self,username,password):pass;
+
+    @staticmethod
+    def checkUsername(username):pass;
+
+    @staticmethod
+    def checkPassword(password):pass;
