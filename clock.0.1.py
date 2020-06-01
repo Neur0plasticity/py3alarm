25df346ea20fd53f7ed5777acfe1ebcaa349804a
djtinkers <djtinkers365@gmail.com>
2020-05-31 21:17:31 -0700

    version 1 complete

---
+++ clock.0.1.py
@@ -0,0 +1,20 @@
+class Clock:
+    """A simple clock written in python"""
+    def __init__(self):
+        pass;
+class Date:
+    """ A simple date obj interface {hour,minute,second} """
+    def __init__(self):
+        pass;
+class Time:
+    """ A simple time obj interface {hour,minute,second}"""
+    def __init__(self):
+        pass;
+class Alarm:
+    """ A simple alarm object interface {dateOBJ,timeOBJ} """
+    def __init__(self):
+        pass;
+class User:
+    """ A simple user object interface {username,password} """
+    def __init__(self):
+        pass;
