fba221030434277b4f85f9ce5cf3561354ce5613
djtinkers <djtinkers365@gmail.com>
2020-05-31 21:11:23 -0700

    version 0 complete

---
+++ clock.0.0.py
@@ -0,0 +1,15 @@
+class Clock:
+    def __init__(self):
+        pass;
+class Date:
+    def __init__(self):
+        pass;
+class Time:
+    def __init__(self):
+        pass;
+class Alarm:
+    def __init__(self):
+        pass;
+class User:
+    def __init__(self):
+        pass;
